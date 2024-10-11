b8d2b7c07e9963c73e61053e0a88a63ec75143a0
Farhad Mamipour <mamipour@gmail.com>
2024-10-11 19:07:57 -0400

    applying pylint

--- server.py
+++ server.py
@@ -1,24 +1,34 @@
+"""
+Flask Server V1.0.0.
+"""
+
 from flask import Flask, render_template, request
-from EmotionDetection.emotion_detection import *
+from EmotionDetection.emotion_detection import emotion_detector
 
 app = Flask("Emotion Detection")
 
 @app.route("/emotionDetector")
 def sent_analyzer():
+    """
+    Emotion Detector main route
+    """
     text_to_analyze = request.args.get('textToAnalyze')
-
     response = emotion_detector(text_to_analyze)
     if response['dominant_emotion'] is None:
         return "Invalid text! Please try again!."
-        
-    return """For the given statement, the system response is 'anger': {}, 'disgust': {}, 'fear': {}, 'joy': {} and 'sadness': {}. The dominant emotion is {}.""".format(
-        response['anger'],response['disgust'],response['fear'],response['joy'],response['sadness'],
-        response['dominant_emotion'])
-
+    return "For the given statement,"+ \
+            "the system response is 'anger': {response['anger']},"+ \
+            " 'disgust': {response['disgust']}, "+ \
+            "'fear': {response['fear']}, 'joy': {response['joy']} "+ \
+            "and 'sadness': {response['sadness']}."+ \
+            "The dominant emotion is {response['dominant_emotion']}."
 
 @app.route("/")
 def render_index_page():
+    """
+    Index route
+    """
     return render_template('index.html')
-    
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
